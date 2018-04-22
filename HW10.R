034960eddcc49cf5561bd1d089565e6639abcd23
carlsonwu <carlsonwu0622@gmail.com>
2018-04-22 16:42:18 -0400

    4:42

--- HW10.R
+++ HW10.R
@@ -1,4 +1,18 @@
 library(shiny)
-ui <- fluidPage()
-server <- function(input, output, session){}
+library(tidyverse)
+library(tidycensus)
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      textInput("State Input", "State(abbr.)"),
+      radioButtons("PlotSelected", "Plot to Show",
+                   choices = c("Median Household Income", "Median Gross Rent", "Ratio"))),
+    mainPanel(
+      plotOutput("Plot")
+    )
+  ),
+  titlePanel("American Community Survey")
+)
+server <- function(input, output, session){
+}
 shinyApp(ui = ui, server = server)
