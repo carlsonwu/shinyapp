942214001e1a03d99c991cbc2e7ecc07a8b3ed5a
carlsonwu <carlsonwu0622@gmail.com>
2018-04-27 18:40:15 -0400

    newest

--- HW10.R
+++ HW10.R
@@ -2,40 +2,41 @@ library(shiny)
 library(choroplethr)
 library(tidyverse)
 library(tidycensus)
+library(ggplot2)
 
 census_api_key("2cb62d6a72a7aba26465acbabf244c2cddf04b20")
 
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      textInput("StateInput", "State(abbr.)"),
-      radioButtons("PlotSelected", "Plot to Show",
-                   choices = c("Median Household Income", "Median Gross Rent", "Ratio"))),
+      selectInput("State", "State",  choices = state.abb),
+      radioButtons("Plot", "Plot",
+                   choices = list("MedianHouseholdIncome", "MedianGrossRent", "Ratio"))),
     mainPanel(
-      plotOutput("main_plot")
+      plotOutput("Plot")
     )
   ),
   titlePanel("American Community Survey")
+  # title
 )
-server <- function(input, output, session){
-  output$main_plot <- renderPlot({
-    selecteddata <- get_acs(geography = "county",
-                            variables = c(medincome = "B19013_001", medrent = "B25064_001"),
-                            state = input$StateInput)
-    selecteddata <- selecteddata %>%
-      mutate(ratio = medrent / medincome)
-    
-    plotselected <- switch(input$PlotSelected,
-                           'Median Household Income' = "medincome",
-                           'Median Gross Rent' = "medrent",
-                           'Ratio' = "ratio")
-    
-    selecteddata %>%
-      select(input$PlotSelected)%>%
-      dplyr::rename(value = estimate) %>%
-      mutate(region = as.integer(GEOID)) %>%
-      choroplethr::country_choropleth(title = input$PlotSelected, num_colors = 1, state_zoom = input$StateInput)
-  }
-  )
+server <- function(input, output){
+ 
+  reduced_df <- reactive({
+    get_acs(
+      geography = "tract",
+      variables = c(MedianHouseholdIncome = "B19013_001", MedianGrossRent = "B25064_001"),
+      state = input$State,
+      geometry = TRUE
+    ) %>%
+      .[,-5] %>%
+      data.frame() %>%
+      spread(key = variable, value = estimate) %>%
+      mutate(ratio = MedianGrossRent / MedianHouseholdIncome)
+  })
+  
+  output$Plot <- renderPlot({
+    reduced_df() %>%
+      ggplot(aes_string(fill = input$Plot)) + scale_fill_gradientn(colours = rainbow(7))
+  })
 }
 shinyApp(ui = ui, server = server)
